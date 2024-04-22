b0a7a566dc69ce34b8634e88101f82952dd63ccf
J0u1es <145380988+J0u1es@users.noreply.github.com>
2024-04-22 15:10:55 -0700

    Did a lot
    Worked on the game. Start on 3.3 lessons.

--- U3SoundAndEffectGonzalezP3/Assets/Scripts/PlayerController.cs
+++ U3SoundAndEffectGonzalezP3/Assets/Scripts/PlayerController.cs
@@ -5,19 +5,36 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
-
+    public float gravityModifier;
+    public float jumpForce;
+    public bool isOnGround = true;
+    public bool gameOver = false;
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space)) 
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround) 
         { 
-            playerRb.AddForce(Vector3.up * 100, ForceMode.Impulse);
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+        }
+    }
+    private void OnCollisionEnter(Collision collision) 
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
+        else if (collision.gameObject.CompareTag("Obstacle"))
+        { 
+            gameOver = true;
+            Debug.Log("Game Over!");
         }
     }
 }
